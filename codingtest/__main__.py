dfcd4b26e36704dbd5ce515086ac22b2fc2205fe
Jakob Huber <jakob-huber@outlook.de>
2025-03-28 13:21:34 +0100

    initial commit

---
+++ codingtest/__main__.py
@@ -0,0 +1,9 @@
+def add() -> int:
+    a = input("Enter a: ")
+    b = input("Enter b: ")
+    return a + b
+
+if __name__ == "__main__":
+    add()
+    
+    
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(
+   name='codingpractice',
+   version='0.1',
+   author="HuberJakob",
+   author_email="jakob.huber@fau.de",
+   packages=find_packages(),
+   install_requires=[],
+)
